0c03d7eae2ea98620c36c7f5d893972e0ebeeb82
xMatysz <matizek56@gmail.com>
2023-06-07 20:04:58 +0200

    add apps config

---
+++ apps.ps1
@@ -0,0 +1,21 @@
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+winget install --id=Git.Git  -e ;
+winget install --id=google.chrome ;
+winget install --id=7zip.7zip -e  ; 
+winget install --id=Microsoft.VisualStudio.2022.Community -e  ; 
+winget install --id=Microsoft.VisualStudioCode -e ;
+winget install --id=JetBrains.Rider -e ;
+winget install --id=Microsoft.PowerShell  -e ;
+winget install --id=Discord.Discord  -e ;
+winget install --id=EpicGames.EpicGamesLauncher  -e ;
+winget install --id=Spotify.Spotify  -e ;
+winget install --id=Valve.Steam  -e ;
+winget install --id=Microsoft.WindowsTerminal  -e ;
+winget install --id=VideoLAN.VLC  -e ;
+winget install --id=Microsoft.PowerToys  -e ;
+winget install --id=dbeaver.dbeaver -e ;
+winget install --id=OBSProject.OBSStudio -e ;
+winget install --id=Debian.Debian -e ;
+winget install --id=Microsoft.DevHome -e ;
+wsl --install ;
